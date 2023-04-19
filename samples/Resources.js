a3fc394fde38c4f9d5925ceec1794d15c5ae25de
eugeneko <eugeneko@users.noreply.github.com>
2023-04-19 12:56:23 +0000

    deploy: rbfx/rbfx@75d5606d2cfe450054720ef7ec4d1545f513d4c4

--- samples/Resources.js
+++ samples/Resources.js
@@ -346,6 +346,6 @@ var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
     }
 
     }
-    loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 751607}, {"filename": "/Data.pak", "start": 751607, "end": 48939531}], "remote_package_size": 48939531, "package_uuid": "sha256-2d9d4af6da3b45f0317cfc4358f65500cb8473e7398ffa4644fa5d0fe521dc37"});
+    loadPackage({"files": [{"filename": "/CoreData.pak", "start": 0, "end": 751609}, {"filename": "/Data.pak", "start": 751609, "end": 48939533}], "remote_package_size": 48939533, "package_uuid": "sha256-5b24b685935a42f51cd71ff6f0a8ede3ac3952e271b53a8a395c46b278b39c64"});
 
   })();
