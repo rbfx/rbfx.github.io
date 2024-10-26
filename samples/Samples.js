7041ccd13f3af58d26a2747762afba000ab652ba
eugeneko <eugeneko@users.noreply.github.com>
2024-10-26 19:13:08 +0000

    deploy: rbfx/rbfx@3aa8952ee4ce165f777088d906871c71a3f19a6d

--- samples/Samples.js
+++ samples/Samples.js
@@ -551,14 +551,14 @@ var tempI64;
 // end include: runtime_debug.js
 // === Body ===
 var ASM_CONSTS = {
-  1946412: () => {
+  1946476: () => {
     FS.syncfs(function(err) {
       if (err) {
         console.error(err);
       }
     });
   },
-  1946476: $0 => {
+  1946540: $0 => {
     var str = UTF8ToString($0) + "\n\n" + "Abort/Retry/Ignore/AlwaysIgnore? [ariA] :";
     var reply = window.prompt(str, "i");
     if (reply === null) {
@@ -566,10 +566,10 @@ var ASM_CONSTS = {
     }
     return allocate(intArrayFromString(reply), "i8", ALLOC_NORMAL);
   },
-  1946701: ($0, $1) => {
+  1946765: ($0, $1) => {
     alert(UTF8ToString($0) + "\n\n" + UTF8ToString($1));
   },
-  1946758: () => {
+  1946822: () => {
     if (typeof (AudioContext) !== "undefined") {
       return true;
     } else if (typeof (webkitAudioContext) !== "undefined") {
@@ -577,7 +577,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  1946905: () => {
+  1946969: () => {
     if ((typeof (navigator.mediaDevices) !== "undefined") && (typeof (navigator.mediaDevices.getUserMedia) !== "undefined")) {
       return true;
     } else if (typeof (navigator.webkitGetUserMedia) !== "undefined") {
@@ -585,7 +585,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  1947139: $0 => {
+  1947203: $0 => {
     if (typeof (Module["SDL2"]) === "undefined") {
       Module["SDL2"] = {};
     }
@@ -607,11 +607,11 @@ var ASM_CONSTS = {
     }
     return SDL2.audioContext === undefined ? -1 : 0;
   },
-  1947632: () => {
+  1947696: () => {
     var SDL2 = Module["SDL2"];
     return SDL2.audioContext.sampleRate;
   },
-  1947700: ($0, $1, $2, $3) => {
+  1947764: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     var have_microphone = function(stream) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -652,7 +652,7 @@ var ASM_CONSTS = {
       }, have_microphone, no_microphone);
     }
   },
-  1949352: ($0, $1, $2, $3) => {
+  1949416: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     SDL2.audio.scriptProcessorNode = SDL2.audioContext["createScriptProcessor"]($1, 0, $0);
     SDL2.audio.scriptProcessorNode["onaudioprocess"] = function(e) {
@@ -664,7 +664,7 @@ var ASM_CONSTS = {
     };
     SDL2.audio.scriptProcessorNode["connect"](SDL2.audioContext["destination"]);
   },
-  1949762: ($0, $1) => {
+  1949826: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var numChannels = SDL2.capture.currentCaptureBuffer.numberOfChannels;
     for (var c = 0; c < numChannels; ++c) {
@@ -683,7 +683,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  1950367: ($0, $1) => {
+  1950431: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var numChannels = SDL2.audio.currentOutputBuffer["numberOfChannels"];
     for (var c = 0; c < numChannels; ++c) {
@@ -696,7 +696,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  1950847: $0 => {
+  1950911: $0 => {
     var SDL2 = Module["SDL2"];
     if ($0) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -734,7 +734,7 @@ var ASM_CONSTS = {
       SDL2.audioContext = undefined;
     }
   },
-  1952019: ($0, $1, $2) => {
+  1952083: ($0, $1, $2) => {
     var w = $0;
     var h = $1;
     var pixels = $2;
@@ -805,7 +805,7 @@ var ASM_CONSTS = {
     }
     SDL2.ctx.putImageData(SDL2.image, 0, 0);
   },
-  1953488: ($0, $1, $2, $3, $4) => {
+  1953552: ($0, $1, $2, $3, $4) => {
     var w = $0;
     var h = $1;
     var hot_x = $2;
@@ -842,19 +842,19 @@ var ASM_CONSTS = {
     stringToUTF8(url, urlBuf, url.length + 1);
     return urlBuf;
   },
-  1954477: $0 => {
+  1954541: $0 => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = UTF8ToString($0);
     }
   },
-  1954560: () => {
+  1954624: () => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = "none";
     }
   },
-  1954629: () => window.innerWidth,
-  1954659: () => window.innerHeight,
-  1954690: $0 => {
+  1954693: () => window.innerWidth,
+  1954723: () => window.innerHeight,
+  1954754: $0 => {
     try {
       const context = GL.getContext($0);
       if (!context) {
