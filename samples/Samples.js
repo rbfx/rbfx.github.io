1d362331ecc7e1cd0fb6d358155b5707ac372e2d
eugeneko <eugeneko@users.noreply.github.com>
2025-03-05 19:49:23 +0000

    deploy: rbfx/rbfx@82468066ceb297cae8579ad3703ed9752e4df02f

--- samples/Samples.js
+++ samples/Samples.js
@@ -466,7 +466,7 @@ async function createWasm() {
 
 // === Body ===
 var ASM_CONSTS = {
-  1949248: $0 => {
+  1949760: $0 => {
     var str = UTF8ToString($0) + "\n\n" + "Abort/Retry/Ignore/AlwaysIgnore? [ariA] :";
     var reply = window.prompt(str, "i");
     if (reply === null) {
@@ -474,10 +474,10 @@ var ASM_CONSTS = {
     }
     return allocate(intArrayFromString(reply), "i8", ALLOC_NORMAL);
   },
-  1949473: ($0, $1) => {
+  1949985: ($0, $1) => {
     alert(UTF8ToString($0) + "\n\n" + UTF8ToString($1));
   },
-  1949530: () => {
+  1950042: () => {
     if (typeof (AudioContext) !== "undefined") {
       return true;
     } else if (typeof (webkitAudioContext) !== "undefined") {
@@ -485,7 +485,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  1949677: () => {
+  1950189: () => {
     if ((typeof (navigator.mediaDevices) !== "undefined") && (typeof (navigator.mediaDevices.getUserMedia) !== "undefined")) {
       return true;
     } else if (typeof (navigator.webkitGetUserMedia) !== "undefined") {
@@ -493,7 +493,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  1949911: $0 => {
+  1950423: $0 => {
     if (typeof (Module["SDL2"]) === "undefined") {
       Module["SDL2"] = {};
     }
@@ -515,11 +515,11 @@ var ASM_CONSTS = {
     }
     return SDL2.audioContext === undefined ? -1 : 0;
   },
-  1950404: () => {
+  1950916: () => {
     var SDL2 = Module["SDL2"];
     return SDL2.audioContext.sampleRate;
   },
-  1950472: ($0, $1, $2, $3) => {
+  1950984: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     var have_microphone = function(stream) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -560,7 +560,7 @@ var ASM_CONSTS = {
       }, have_microphone, no_microphone);
     }
   },
-  1952124: ($0, $1, $2, $3) => {
+  1952636: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     SDL2.audio.scriptProcessorNode = SDL2.audioContext["createScriptProcessor"]($1, 0, $0);
     SDL2.audio.scriptProcessorNode["onaudioprocess"] = function(e) {
@@ -572,7 +572,7 @@ var ASM_CONSTS = {
     };
     SDL2.audio.scriptProcessorNode["connect"](SDL2.audioContext["destination"]);
   },
-  1952534: ($0, $1) => {
+  1953046: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var numChannels = SDL2.capture.currentCaptureBuffer.numberOfChannels;
     for (var c = 0; c < numChannels; ++c) {
@@ -591,7 +591,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  1953139: ($0, $1) => {
+  1953651: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var numChannels = SDL2.audio.currentOutputBuffer["numberOfChannels"];
     for (var c = 0; c < numChannels; ++c) {
@@ -604,7 +604,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  1953619: $0 => {
+  1954131: $0 => {
     var SDL2 = Module["SDL2"];
     if ($0) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -642,7 +642,7 @@ var ASM_CONSTS = {
       SDL2.audioContext = undefined;
     }
   },
-  1954791: ($0, $1, $2) => {
+  1955303: ($0, $1, $2) => {
     var w = $0;
     var h = $1;
     var pixels = $2;
@@ -713,7 +713,7 @@ var ASM_CONSTS = {
     }
     SDL2.ctx.putImageData(SDL2.image, 0, 0);
   },
-  1956260: ($0, $1, $2, $3, $4) => {
+  1956772: ($0, $1, $2, $3, $4) => {
     var w = $0;
     var h = $1;
     var hot_x = $2;
@@ -750,19 +750,19 @@ var ASM_CONSTS = {
     stringToUTF8(url, urlBuf, url.length + 1);
     return urlBuf;
   },
-  1957249: $0 => {
+  1957761: $0 => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = UTF8ToString($0);
     }
   },
-  1957332: () => {
+  1957844: () => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = "none";
     }
   },
-  1957401: () => window.innerWidth,
-  1957431: () => window.innerHeight,
-  1957462: $0 => {
+  1957913: () => window.innerWidth,
+  1957943: () => window.innerHeight,
+  1957974: $0 => {
     try {
       const context = GL.getContext($0);
       if (!context) {
